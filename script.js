7a6f6c0a7b70b94b60e8cecf18e5f4405ed5c2e9
rajeshycode <136836225+rajeshycode@users.noreply.github.com>
2023-09-26 06:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-function threeSum(arr, target) {
-  if (arr.length < 3) {
-    throw new Error("Input array should have at least three integers.");
-  }
-
-  arr.sort((a, b) => a - b); // Sort the array in ascending order.
-
-  let closestSum = Infinity;
-
-  for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
-
-    while (left < right) {
-      const currentSum = arr[i] + arr[left] + arr[right];
-
-      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-        closestSum = currentSum;
-      }
-
-      if (currentSum < target) {
-        left++;
-      } else {
-        right--;
-      }
-    }
-  }
-
-  return closestSum;
-}
-
-module.exports = threeSum;
+const arr = [-1, 2, 1, -4];
+const target = 1;
+console.log(threeSum(arr, target)); // Output: 2
